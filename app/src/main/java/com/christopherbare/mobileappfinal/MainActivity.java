bdbd4b782acdda6a6f206936422789e2e160dbc8
Christopher Bare <christopherbare@outlook.com>
2018-08-05 15:45:51 -0400

    Switched to recyclerView

--- app/src/main/java/com/christopherbare/mobileappfinal/MainActivity.java
+++ app/src/main/java/com/christopherbare/mobileappfinal/MainActivity.java
@@ -8,22 +8,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Floating action button", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-    }
 
+
+
+
+
+
+    }
 }
