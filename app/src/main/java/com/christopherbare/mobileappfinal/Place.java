41c2babcf0d22c50713c92b695a4d6ac19f10d79
Christopher Bare <christopherbare@outlook.com>
2018-08-05 20:22:56 -0400

    Added some stings

--- app/src/main/java/com/christopherbare/mobileappfinal/Place.java
+++ app/src/main/java/com/christopherbare/mobileappfinal/Place.java
@@ -1,6 +1,15 @@
 package com.christopherbare.mobileappfinal;
 
 public class Place {
+String place;
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
 
     public Place() {
     }
