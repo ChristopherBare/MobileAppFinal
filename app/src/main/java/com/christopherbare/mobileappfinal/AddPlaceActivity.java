a4ef18cac83b8fa22f96d6c79d611d339a7f5b53
Christopher Bare <christopherbare@outlook.com>
2018-08-07 00:49:16 -0400

    something else

--- app/src/main/java/com/christopherbare/mobileappfinal/AddPlaceActivity.java
+++ app/src/main/java/com/christopherbare/mobileappfinal/AddPlaceActivity.java
@@ -62,9 +62,7 @@ public class AddPlaceActivity extends AppCompatActivity {
             StringBuilder stringBuilder = new StringBuilder();
             HttpURLConnection connection = null;
             String result = null;
-            try
-
-            {
+            try {
                 String strUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" +
                         "AIzaSyDibjKMrJAEjxFgTDhZUnGcu9mNYcwkXNQ"
                         + "&location=" +
@@ -105,6 +103,68 @@ public class AddPlaceActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             Toast.makeText(AddPlaceActivity.this, s, Toast.LENGTH_SHORT).show();
+            //TODO Fix this to get the places nearby using the lat and lng that was gotten from the other async class
+            try{
+                JSONObject object = new JSONObject(s);
+                JSONArray predictions = object.getJSONArray("predictions");
+
+                for(int i = 0; i < predictions.length(); i++){
+                    JSONArray terms = predictions.getJSONObject(i).getJSONArray("terms");
+                    String place = terms.getJSONObject(0).get("value") + ", " + terms.getJSONObject(1).get("value");
+                    //places.add(place);
+                }
+            } catch (JSONException e) {
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    private class GetLocationAsync extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder stringBuilder = new StringBuilder();
+            HttpURLConnection connection = null;
+            String result = null;
+            try {
+                String strUrl = "https://maps.googleapis.com/maps/api/place/details/json?key=" +
+                        "AIzaSyDibjKMrJAEjxFgTDhZUnGcu9mNYcwkXNQ"
+                        + placeID;
+                URL url = new URL(strUrl);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    result = IOUtils.toString(connection.getInputStream(), "UTF8");
+                    Log.i("Tag", "internet");
+                } else {
+                    Log.i("Tag", "no internet" + connection.getResponseCode());
+
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                Log.i("Tag", "errror");
+            } catch (IOException e)
+
+            {
+                e.printStackTrace();
+                Log.i("Tag", "errror");
+            } //Handle the exceptions
+            finally
+
+            {
+                //Close open connections and reader
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(AddPlaceActivity.this, s, Toast.LENGTH_SHORT).show();
+           //TODO fix this stuff to get the lat and lng and set those variables
             try{
                 JSONObject object = new JSONObject(s);
                 JSONArray predictions = object.getJSONArray("predictions");
